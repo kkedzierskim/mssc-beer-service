fd8267622b9a006959bcde6d5ed1070a450138bd
Mateusz Kędzierski <mateusz.kedzierski@execon.pl>
2021-11-14 12:15:20 +0100

    beer bootstrap data

--- src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -1,9 +1,9 @@
 package guru.springframework.msscbeerservice.repositories;
 
-import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.domain.Beer;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<BeerDto, UUID> {
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
 }
